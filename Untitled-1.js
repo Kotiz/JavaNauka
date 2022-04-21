ae3ad9144ceb2315c3fbfe5a2228fdc450748abc
Kotiz <analogiasnu@gmail.com>
2022-04-21 15:55:41 +0200

    funkcje

--- Untitled-1.js
+++ Untitled-1.js
@@ -33,10 +33,25 @@
 
                             // LOOP IN []
 
-const age=[23,19,45,24,78,34];
-const name=["Ala","Ela","Ola","Nina","Maryla","Ania"]
+// const age=[23,19,45,24,78,34];
+// const name=["Ala","Ela","Ola","Nina","Maryla","Ania"];
 
-console.log("LP.", "Wiek \t", "Imie");
-for (let i = 0; i < age.length; i++ ) {
-    console.log(`${i+1} \t ${age[i]} \t ${name[i]}`);   
-}
+// console.log("LP.\t", "Wiek \t\t", "Imie");
+// for (let i = 0; i < age.length; i++ ) {
+//     console.log(`${i+1} \t ${age[i]} lat \t ${name[i]}`);   
+// }
+
+                            // LOOP FOR from ES6
+
+// const names=["Ala","Ela","Ola","Nina","Maryla","Ania"];
+// for (const name of names) {
+//     console.log(name);
+// }
+
+                            // FUNKCJE //
+
+const showText = (text) => {
+    const insideShowText = text;
+    return (insideShowText)
+}  ;
+insideShowText2 = showText("Hello");
